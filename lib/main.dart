fe218251ddff015d2b3d18df518072320f82853e
Digital <digital@LAMAR-B05.local>
2022-01-18 16:23:57 +0100

    J2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'dart:async';
 
+import 'package:just_audio/just_audio.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -31,36 +33,152 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 100;
+  int _counter = 0;
   int _autoclickNumbers = 1;
-  int _bonusMultiplicator = 1;
+  int _bonusMultiplicatorNumbers = 1;
 
-  String _ferrariImageName(){
+
+  bool _playDinoSound = true;
+  bool _play458ItaliaSound = true;
+  bool _playTestarossaSound = true;
+  bool _play360modenaSound = true;
+  bool _playEnzoSound = true;
+  bool _play812SuperfastSound = true;
+  bool _playF40Sound = true;
+  bool _playLaFerrariSound = true;
+  bool _play250GTOSound = true;
+  bool _playMonzaSound = true;
+
+
+
+  _ferrariImageName(){
     if(_counter < 10){
+      if(_playDinoSound){
+        _playDinoSound = false;
+        _playDino();
+      }
       return "images/dino.png";
     }else if(_counter < 100){
-      return "images/250GTO.png";
+      if(_play458ItaliaSound){
+        _play458ItaliaSound = false;
+        _play458Italia();
+      }
+      return "images/458Italia.png";
     }else if(_counter < 500) {
+      if(_playTestarossaSound){
+        _playTestarossaSound = false;
+        _playTestarossa();
+      }
       return "images/testarossa.png";
     }else if(_counter < 1000) {
+      if(_play360modenaSound){
+        _play360modenaSound = false;
+        _play360modena();
+      }
       return "images/360modena.png";
     }else if(_counter < 5000){
+      if(_playEnzoSound){
+        _playEnzoSound = false;
+        _playEnzo();
+      }
       return "images/enzo.png";
     }else if(_counter < 10000){
+      if(_play812SuperfastSound) {
+        _play812SuperfastSound = false;
+        _play812Superfast();
+      }
       return "images/812superfast.png";
     }else if(_counter < 50000){
+      if(_playF40Sound){
+        _playF40Sound = false;
+        _playF40();
+      }
       return "images/F40.png";
     }else if(_counter < 100000){
+      if(_playLaFerrariSound){
+        _playLaFerrariSound = false;
+        _playLaFerrari();
+      }
       return "images/laFerrari.png";
-    }else if(_counter < 50000) {
-      return "images/458Italia.png";
+    }else if(_counter < 500000) {
+      if(_play250GTOSound){
+        _play250GTOSound = false;
+        _play250GTO();
+      }
+      return "images/250GTO.png";
+    }else if(_counter >= 50000){
+      if(_playMonzaSound) {
+        _playMonzaSound = false;
+        _playMonza();
+      }
+      return "images/monza.png";
     }
-    return "images/monza.png";
+  }
+
+  Future<void> _playDino() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/dino.mp3');
+    player.setClip(start: Duration(seconds: 15), end: Duration(seconds: 20));
+    player.play();
+  }
+  Future<void> _play458Italia() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/458Italia.mp3');
+    player.setClip(start: Duration(seconds: 0), end: Duration(seconds: 3));
+    player.play();
+  }
+  Future<void> _playTestarossa() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/testarossa.mp3');
+    player.setClip(start: Duration(seconds: 32), end: Duration(seconds: 35));
+    player.play();
+  }
+  Future<void> _play360modena() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/360modena.mp3');
+    player.setClip(start: Duration(seconds: 2), end: Duration(seconds: 5));
+    player.play();
+  }
+  Future<void> _playEnzo() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/enzo.mp3');
+    player.setClip(start: Duration(seconds: 146), end: Duration(seconds: 150));
+    player.play();
+  }
+  Future<void> _play812Superfast() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/812superfast.mp3');
+    player.setClip(start: Duration(seconds: 6), end: Duration(seconds: 10));
+    player.play();
+  }
+  Future<void> _playF40() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/F40.mp3');
+    player.setClip(start: Duration(seconds: 0), end: Duration(seconds: 10));
+    player.play();
+  }
+  Future<void> _playLaFerrari() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/laFerrari.mp3');
+    player.setClip(start: Duration(seconds: 35), end: Duration(seconds: 39));
+    player.play();
+  }
+  Future<void> _play250GTO() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/250GTO.mp3');
+    player.setClip(start: Duration(seconds: 13), end: Duration(seconds: 19));
+    player.play();
+  }
+  Future<void> _playMonza() async {
+    final player = AudioPlayer();
+    var duration = await player.setAsset('sounds/monza.mp3');
+    player.setClip(start: Duration(seconds: 8), end: Duration(seconds: 14));
+    player.play();
   }
 
   void _incrementCounter() {
     setState(() {
-      _counter = _counter + 1;
+      _counter = _counter + (1*_bonusMultiplicatorNumbers);
     });
   }
 
@@ -84,13 +202,14 @@ class _MyHomePageState extends State<MyHomePage> {
 
   void _buyBonusMultiplicator(){
     setState(() {
-      var cost = 100 * _bonusMultiplicator;
+      var cost = 100 * _bonusMultiplicatorNumbers;
       if(_counter >= cost){
         _counter = _counter - cost;
-        _bonusMultiplicator++;
+        _bonusMultiplicatorNumbers++;
       }
     });
   }
+
 
   void _initGame() async{
     _autoclickTimer();
@@ -106,8 +225,6 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Container(
           decoration: BoxDecoration(
             image: DecorationImage(
@@ -146,7 +263,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child:Image.asset("images/cursor.png",height:80,width:80,),
                     ),
                       Text(
-                          '${_autoclickNumbers}/s : ${_autoclickNumbers*100} HP',
+                          '${_autoclickNumbers}/s : ${_autoclickNumbers*50} HP',
                           style: GoogleFonts.domine(
                             textStyle: TextStyle(color:Colors.white, fontSize:20,),
                           )
@@ -161,7 +278,7 @@ class _MyHomePageState extends State<MyHomePage> {
                       child:Image.asset("images/pointer.png",height:80,width:80,),
                     ),
                       Text(
-                          'X${_bonusMultiplicator} : ${_bonusMultiplicator*100} HP',
+                          'X${_bonusMultiplicatorNumbers+1} : ${_bonusMultiplicatorNumbers*100} HP',
                           style: GoogleFonts.domine(
                             textStyle:TextStyle(color:Colors.white, fontSize:20,),
                           )
@@ -173,11 +290,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
 
             ],
-          ) /* add child content here */,
+          ),
         ),
       ),
-
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
